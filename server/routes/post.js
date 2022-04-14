4eaa08779e5169cdf87fb6f94ab4bfa08a5ba4e0
SuperShubh <shubhpatel179@gmail.com>
2022-04-14 21:32:16 +0530

    adding files

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,7 +1,14 @@
-import express  from 'express';
-import {getpost,createpost} from '../controllers/post.js';
-const router =express.Router();
-router.get('/',getpost);
-router.post('/',createpost);
+import express from 'express';
+
+import { getPosts, getPost, createPost, updatePost, likePost, deletePost } from '../controllers/posts.js';
+
+const router = express.Router();
+
+router.get('/', getPosts);
+router.post('/', createPost);
+router.get('/:id', getPost);
+router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
+router.patch('/:id/likePost', likePost);
 
 export default router;
